6e45ba57c8192f7c0ffa58e856a0d0c583af824f
Douglas <61967200+mattssondouglas@users.noreply.github.com>
2022-10-31 13:24:29 +0100

    CHANGING TO FUNCTIONAL UNSTABLE

--- src/App.js
+++ src/App.js
@@ -1,61 +1,89 @@
-import { Component } from 'react';
+import {useState} from 'react'
+
 import CardList from './components/card-list/card-list.component.jsx'
 import SearchBox from './components/search-box/search-box.component.jsx'
 import './App.css';
 
-class App extends Component {
+const App = () => {
+	const [searchField, setSearchField] = useState('') // [value, setValue]
+	const [prospects, setProspects] = useState([])
 
-	constructor() {
-		super()
-
-		this.state = {
-			prospects : [],
-			searchField: '',
-		}
-	}
-
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then((res) => res.json())
-			.then((users) =>
-				this.setState(
-					() => {
-						return { prospects: users }
-			},
-			() => {
-
-			}
-		)
+	fetch('https://jsonplaceholder.typicode.com/users')
+	.then((res) => res.json())
+	.then((users) => setProspects(users)
 	)
+
+	const onSearchChange =  (event) => {
+		const searchFieldString = event.target.value.toLocaleLowerCase()
+		setSearchField(searchFieldString)
 	}
 
-	onSearchChange = (event) => {
+	const filteredProspects = prospects.filter( (prospect) => {
+		return prospect.name.toLocaleLowerCase().includes(searchField)
+	})
 
-		const searchField = event.target.value.toLocaleLowerCase()
+	return (
+		<div className='App'>
+		<h1 className='app-title'>Prospect Finder</h1>
+		<SearchBox onChangeHandler={onSearchChange} placeholder='Search prospects...' className="prospects-search-box"/>
+		<CardList prospects={filteredProspects}/>
+		</div>
+	)
+}
 
-		this.setState( () => {
-			return { searchField }
-		})
-	}
+// class App extends Component {
 
-		render() {
+// 	constructor() {
+// 		super()
 
-			const { prospects, searchField } = this.state;
-			const { onSearchChange } = this;
+// 		this.state = {
+// 			prospects : [],
+// 			searchField: '',
+// 		}
+// 	}
 
-			const filteredProspects = prospects.filter( (prospect) => {
-				return prospect.name.toLocaleLowerCase().includes(searchField)
-			})
+	// componentDidMount() {
+	// 	fetch('https://jsonplaceholder.typicode.com/users')
+	// 		.then((res) => res.json())
+	// 		.then((users) =>
+	// 			this.setState(
+	// 				() => {
+	// 					return { prospects: users }
+	// 		},
+	// 		() => {
+
+	// 		}
+	// 	)
+	// )
+// 	}
+
+// 	onSearchChange = (event) => {
+
+// 		const searchField = event.target.value.toLocaleLowerCase()
+
+// 		this.setState( () => {
+// 			return { searchField }
+// 		})
+// 	}
+
+// 		render() {
+
+// 			const { prospects, searchField } = this.state;
+// 			const { onSearchChange } = this;
+
+// 			const filteredProspects = prospects.filter( (prospect) => {
+// 				return prospect.name.toLocaleLowerCase().includes(searchField)
+// 			})
 
 
-			return (
-				<div className='App'>
-				<h1 className='app-title'>Prospect Finder</h1>
-				<SearchBox onChangeHandler={onSearchChange} placeholder='Search prospects...' className="prospects-search-box"/>
-				<CardList prospects={filteredProspects}/>
-				</div>
-			)
-		}}
+// 			return (
+// 				<div className='App'>
+// 				<h1 className='app-title'>Prospect Finder</h1>
+// 				<SearchBox onChangeHandler={onSearchChange} placeholder='Search prospects...' className="prospects-search-box"/>
+// 				<CardList prospects={filteredProspects}/>
+// 				</div>
+// 			)
+// 		}}
 
 
 export default App;
